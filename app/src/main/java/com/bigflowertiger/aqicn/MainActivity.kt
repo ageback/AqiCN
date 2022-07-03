ccfbdde0802dffdd44ce6da80286b72e0f13718f
Michael Lu <yeskky@gmail.com>
2022-07-03 21:10:58 +0800

    update

--- app/src/main/java/com/bigflowertiger/aqicn/MainActivity.kt
+++ app/src/main/java/com/bigflowertiger/aqicn/MainActivity.kt
@@ -9,9 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
